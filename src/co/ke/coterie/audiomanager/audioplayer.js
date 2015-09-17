1ead45a9a0ebc9fc83f42da8eecd771d0ced4fdc
Ben <ben@coterie.co.ke>
2015-09-17 17:14:24 +0300

    extend the audiomanager functionality

--- src/co/ke/coterie/audiomanager/audioplayer.js
+++ src/co/ke/coterie/audiomanager/audioplayer.js
@@ -2,17 +2,55 @@
  * @package   Audio Player
  * @author    Ben <ben@coterie.co.ke>
  * @link      http://coterie.co.ke
+ * Extends co.ke.coterie.audio.Manager and provides the UI aspect
  */
- 
-goog.provide('co.ke.coterie.audio.Sound');
+
+goog.provide('audioPlayer');
+goog.provide('co.ke.coterie.audio.Player');
+
+goog.require('co.ke.coterie.audio.Sound');
+goog.require('co.ke.coterie.audio.Manager');
 
 
 /**
  * @constructor
- * @extends {goog.events.EventTarget}
+ * @extends {co.ke.coterie.audio.Manager}
  * This class will manage the interaction between the UI and the audio manager
 */
-co.ke.coterie.audio.Sound = function()
+co.ke.coterie.audio.Player = function()
 {
-	
+	co.ke.coterie.audio.Manager.call(this);
 }
+
+goog.inherits( co.ke.coterie.audio.Player, co.ke.coterie.audio.Manager );
+
+/**
+ * @type {goog.ui.Control}
+ */
+co.ke.coterie.audio.Player.prototype.playButton;
+
+/**
+ * @type {goog.ui.Control}
+ */
+co.ke.coterie.audio.Player.prototype.nextButton;
+
+/**
+ * @type {goog.ui.Control}
+ */
+co.ke.coterie.audio.Player.prototype.prevButton;
+
+/**
+ * @type {goog.ui.Control}
+ */
+co.ke.coterie.audio.Player.prototype.volumeSlider;
+
+/**
+ * @type {goog.ui.Control}
+ */
+co.ke.coterie.audio.Player.prototype.seekBar;
+
+
+
+audioPlayer = new co.ke.coterie.audio.Player;
+
+goog.exportSymbol( 'audioPlayer', audioPlayer );
