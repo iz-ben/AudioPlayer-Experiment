578538feb1d0596c10286c7d1dec167a7913608c
Ben <ben@coterie.co.ke>
2015-09-17 16:09:13 +0300

    Flesh out the sound construct

--- src/co/ke/coterie/audiomanager/sound.js
+++ src/co/ke/coterie/audiomanager/sound.js
@@ -2,6 +2,9 @@
  * @package   Audio Manager
  * @author    Ben <ben@coterie.co.ke>
  * @link      http://coterie.co.ke
+ * References
+ *  - https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createMediaElementSource
+ *  - https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createAnalyser
  */
  
 goog.provide('co.ke.coterie.audio.Sound');
@@ -26,6 +29,8 @@ co.ke.coterie.audio.Sound = function( audioManager, source_url, title )
 	this.setTitle( title );
 	
 	this.streamUrl = new goog.Uri( source_url );
+	
+	this.setup();
 }
 
 /**
@@ -49,6 +54,37 @@ co.ke.coterie.audio.Sound.prototype.title_ = '';
  * @type {goog.Uri}
  */
 co.ke.coterie.audio.Sound.prototype.streamUrl;
+
+/**
+ * @type {Audio}
+ */
+co.ke.coterie.audio.Sound.prototype.audio_;
+
+/**
+ * @type {AudioContext}
+ */
+co.ke.coterie.audio.Sound.prototype.audioContext_;
+
+/**
+ * @type {AudioGain}
+ */
+co.ke.coterie.audio.Sound.prototype.gain_;
+
+/**
+ * @type {RealtimeAnalyserNode}
+ */
+co.ke.coterie.audio.Sound.prototype.analyser_;
+
+/**
+ * @type {MediaElementAudioSourceNode}
+ */
+co.ke.coterie.audio.Sound.prototype.mediaElementSource_;
+
+/**
+ * Animation frame id
+ * @type {number}
+ */
+co.ke.coterie.audio.Sound.prototype.animationFrame = 0;
 
 /**
  * @param {number} volume
@@ -80,4 +116,88 @@ co.ke.coterie.audio.Sound.prototype.setTitle = function( title )
 co.ke.coterie.audio.Sound.prototype.getTitle = function()
 {
 	return this.title_;
+}
+
+/**
+ * @return {Audio}
+ */
+co.ke.coterie.audio.Sound.prototype.getAudio = function()
+{
+	//create new Audio instance on demand if being fetched for the first time
+	this.audio_ = this.audio_ || new Audio;
+	
+	return this.audio_;
+}
+
+/**
+ * @return {AudioGain}
+ */
+co.ke.coterie.audio.Sound.prototype.getGain = function()
+{
+	return this.gain_;
+}
+
+/**
+ * @return {AudioContext}
+ */
+co.ke.coterie.audio.Sound.prototype.getContext = function()
+{
+	this.audioContext_ = this.audioContext_ || new AudioContext;
+	return this.audioContext_;
+}
+
+/**
+ * @return {RealtimeAnalyserNode}
+ */
+co.ke.coterie.audio.Sound.prototype.getAnalyser = function()
+{
+	return this.analyser_;
+}
+
+/**
+ * @return {MediaElementAudioSourceNode}
+ */
+co.ke.coterie.audio.Sound.prototype.getElementSource = function()
+{
+	
+	return this.mediaElementSource_;
+}
+
+
+co.ke.coterie.audio.Sound.prototype.setup = function()
+{
+	//setup Audio instance	
+	
+	this.getAudio().controls = false;
+	
+	this.getAudio().autoplay = false;
+	
+	//setup Contexts
+	
+	this.mediaElementSource_ = this.getContext().createMediaElementSource( this.getAudio() );
+	this.gain_ = this.getContext().createGain();
+	this.analyser_ = this.getContext().createAnalyser();
+	
+	// setup audio filter paths
+	this.getElementSource().connect( this.getGain() );
+	
+	this.getGain().connect( this.getAnalyser() );
+	
+	this.getAnalyser().connect( this.getContext().destination );
+	
+	this.getAudio().crossOrigin = 'anonymous';
+	
+	this.getAudio().src = this.streamUrl.toString();
+}
+
+co.ke.coterie.audio.Sound.prototype.draw = function()
+{
+	this.animationFrame = window.requestAnimationFrame( goog.bind( this.draw, this ) );
+}
+
+co.ke.coterie.audio.Sound.prototype.play = function()
+{
+	this.animationFrame = window.requestAnimationFrame( goog.bind( this.draw, this ) );
+	
+	this.getAudio().play();
 }
